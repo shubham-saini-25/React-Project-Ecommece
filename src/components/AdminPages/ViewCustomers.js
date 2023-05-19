9ca9edf5f3f314eb5d07d1bb84df95140030d3fa
Shubham Saini <shubhamsaini5878@gmail.com>
2023-05-19 17:35:35 +0530

    add admin features

--- src/components/AdminPages/ViewCustomers.js
+++ src/components/AdminPages/ViewCustomers.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import { Container, Button, Modal } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import axios from 'axios';
 
